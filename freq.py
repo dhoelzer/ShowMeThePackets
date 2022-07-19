2daaefe02d08bfe89c76bf4a59c46f6b69575d8b
David Hoelzer <dhoelzer@enclaveforensics.com>
2022-07-19 12:14:08 -0400

    Update packet freq script to support writing to a file.

--- freq.py
+++ freq.py
@@ -5,7 +5,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Generate time domain and frequency domain plots of the input data.")
 parser.add_argument('-w','--window', default=60.0, type=float, dest="window", help="Define the time window for the time domain histogram and frequency analysis.")
+parser.add_argument('-f', '--file', default=None, type=str, dest="output_filename", help="Output the graph to a file rather than to the screen.")
 window = parser.parse_args().window
+output_filename = parser.parse_args().output_filename
 bin=0
 startingTime = 0.0
 endingTime = 0.0
@@ -38,5 +40,8 @@ seconds = [float(n) * window for n in range(0,len(timeDomain))]
 plt.plot(seconds,timeDomain)
 plt.title("Packets over time")
 plt.xlabel("Seconds")
-plt.show()
+if(output_filename):
+	plt.savefig(output_filename, dpi=150, format='png')
+else:
+	plt.show()
 
